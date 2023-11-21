5a1ce8cde96850f0f03d10d276f7ff5b16d52e9e
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2023-11-21 22:29:42 +0100

    Add show() function to Journeys class and implement destructor

--- journeys.cpp
+++ journeys.cpp
@@ -1,1 +1,32 @@
 #include "journeys.h"
+#include <iostream>
+
+void Journeys::show() const {
+  NodeJourney *nodeJourney = _first;
+  while (nodeJourney) {
+    std::cout << nodeJourney->journey;
+    nodeJourney = nodeJourney->next;
+    if (nodeJourney)
+      std::cout << " | ";
+  }
+}
+
+std::ostream &operator<<(std::ostream &os, const Journeys &journeys) {
+  journeys.show();
+  return os;
+}
+
+void Journeys::add(Journey *journey) {
+  _last = _last->next = new NodeJourney{journey, nullptr};
+  if (!_first)
+    _first = _last;
+}
+
+Journeys::~Journeys() {
+  while (_first) {
+    _last = _first; // from here on, _last is only used as a temp var
+    _first = _first->next;
+    delete _last->journey;
+    delete _last;
+  }
+}
--- journeys.h
+++ journeys.h
@@ -3,6 +3,21 @@
 
 #include "journey.h"
 
+struct NodeJourney {
+  Journey *journey;
+  NodeJourney *next;
+};
 
+class Journeys {
+public:
+  void show() const;
+  friend std::ostream &operator<<(std::ostream &os, const Journeys &journeys);
+  void add(Journey *journey);
+  virtual ~Journeys();
+
+protected:
+  NodeJourney *_first = nullptr;
+  NodeJourney *_last = nullptr;
+};
 
 #endif
