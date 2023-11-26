00aea323243d5e456ab2e55bb4a5a02b96fda3b2
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2023-11-26 11:26:57 +0100

    private SimpleJourney class and fix formatting

--- simpleJourney.h
+++ simpleJourney.h
@@ -3,18 +3,18 @@
 
 #include "journey.h"
 
-
-class SimpleJourney:public Journey {
+class SimpleJourney : public Journey {
 protected:
   const char *_transportMethod;
 
 public:
-  SimpleJourney(const char *start = "", const char *end = "",const char *transportMethod = "");
-
-  void show(void) const;
+  SimpleJourney(const char *start = "", const char *end = "",
+                const char *transportMethod = "");
 
   virtual ~SimpleJourney();
-};
 
+private:
+  void show(void) const;
+};
 
 #endif
