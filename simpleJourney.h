c5b117c576f9d12c1d62e9acf63ddbb0a6c0bef3
CLEMS3 <clement.chapard@insa-lyon.fr>
2023-11-27 17:43:42 +0100

    ajout des commentaires du squelette sur simpleJourney

--- simpleJourney.h
+++ simpleJourney.h
@@ -1,19 +1,44 @@
+/*************************************************************************
+                           SimpleJourney  -  description
+                             -------------------
+    début                : 27/11/2023
+    copyright            : (C) 2023 par Jixiang, Adam, Clément, Louis
+    binome               : B3311 et B3309
+*************************************************************************/
+
+//--- Interface de la classe <simpleJourney> (fichier simpleJourney.h) ---
+
 #ifndef SIMPLEJOURNEY_H_
 #define SIMPLEJOURNEY_H_
+
+//--------------------------------------------------- Interfaces utilisées
 
 #include "journey.h"
 
 class SimpleJourney : public Journey {
-protected:
-  const char *_transportMethod;
+
+//----------------------------------------------------------------- PUBLIC
 
 public:
-  SimpleJourney(const char *start = "", const char *end = "",
-                const char *transportMethod = "");
+
+//-------------------------------------------- Constructeurs - destructeur
+
+  SimpleJourney(const char *start = "", const char *end = "", const char *transportMethod = "");
 
   virtual ~SimpleJourney();
 
+//-------------------------------------------------------------- PROTECTED
+
+protected:
+//----------------------------------------------------- Attributs protégés
+
+  const char *_transportMethod;
+
+//------------------------------------------------------------------ PRIVE
+
 private:
+//------------------------------------------------------- Méthodes privées
+
   void show(void) const;
 };
 
