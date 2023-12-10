14041cf460243216060d99b05e317d66929f11b5
AdSchl2E <89904512+AdSchl2E@users.noreply.github.com>
2023-12-10 12:05:22 +0100

    Update catalog.h //Add comment

--- catalog.h
+++ catalog.h
@@ -6,8 +6,7 @@
     binome               : B3311 et B3309
 *************************************************************************/
 
-//---------- Interface de la classe <Catalog> (fichier catalog.h)
-//----------------
+//---------- Interface de la classe <Catalog> (fichier catalog.h) ----------------
 #ifndef CATALOG_H_
 #define CATALOG_H_
 
@@ -15,34 +14,62 @@
 #include "journeys.h"
 
 struct PathNode {
+  // Attributes :
+  // The journey that this pathNode represents
+  // The last pathNode of the path to this journey
+  // The next pathNodes of the path to this journey
+  // The number of references to this pathNode (used to know if the pathNode is still used or not and if it can be deleted or not)
   Journey *pjourney;
   PathNode *lastPathNode;
   LinkedList<PathNode> nextPathNodes;
   unsigned int refCount = 0;
 
+  // Methods :
   // Constructor that takes two parameters
   PathNode(Journey *journey, PathNode *lastNode)
       : pjourney(journey), lastPathNode(lastNode) {}
 };
 
 class Catalog : public Journeys {
-  //----------------------------------------------------------------- PUBLIC
+//----------------------------------------------------------------- PUBLIC
 
 public:
-  //----------------------------------------------------- Méthodes publiques
+//----------------------------------------------------- Méthodes publiques
 
   void Search(const char *const from, const char *const to,
               PathNode *pathHead = nullptr);
-  //------------------------------------------------- Surcharge d'opérateurs
+  // Mode d'emploi :
+
+  // This function will search all the possible journeys from the given
+  // departure to the given arrival and print them.
+  // from : the departure
+  // to : the arrival
+  // pathNode : the pathNode that contains the path to the current journey
+
+  // A DFS Search is used to find all the possible journeys.
+
+  // Contrat :
+
+  // The pathNode must be a valid pathNode of the catalog.
+  // The pathNode must be the pathNode of the journey that is currently
+  // being searched.
+
+
+//------------------------------------------------- Surcharge d'opérateurs
 
   friend std::ostream &operator<<(std::ostream &os, const Catalog &Catalog);
+  // Mode d'emploi :
+  // This function will print the catalog in the given ostream.
+  // os : the ostream where the catalog will be printed
+  // Catalog : the catalog that will be printed
 
-  //------------------------------------------------------------------ PRIVE
+//------------------------------------------------------------------ PRIVE
 
 protected:
-  //----------------------------------------------------- Attributs protégés
+//----------------------------------------------------- Attributs protégés
 
   PathNode pathRoot{nullptr, nullptr};
+  
 };
 
 #endif
