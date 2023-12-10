d926691c9fbfb838fff267ccb8cd813634ab938b
AdSchl2E <89904512+AdSchl2E@users.noreply.github.com>
2023-12-10 12:32:02 +0100

    Update journeys.cpp //Add comment

--- journeys.cpp
+++ journeys.cpp
@@ -6,8 +6,7 @@
     binome               : B3311 et B3309
 *************************************************************************/
 
-//---------- Réalisation de la classe <journeys> (fichier journeys.cpp)
-//----------
+//---------- Réalisation de la classe <journeys> (fichier journeys.cpp) ----------
 
 //---------------------------------------------------------------- INCLUDE
 
@@ -30,7 +29,15 @@ ostream &operator<<(ostream &os, const Journeys &journeys) {
 //------------------------------------------------------------------ PRIVE
 
 //----------------------------------------------------- Méthodes privées
-void Journeys::show(const char sep) const {
+void Journeys::show(const char sep) const 
+// Algorithme :
+// Show all the journeys in the journeys
+// sep : the separator between each journey
+// If sep is '\n', the function will print the index of the journey
+// before printing the journey
+// If sep is not '\n', the function will not print the index of the journey
+// before printing the journey
+{
   Node<Journey> *nodeJourney = journeyLinkedList.GetFirst();
 
   for (unsigned int i = 1; nodeJourney; i++) {
